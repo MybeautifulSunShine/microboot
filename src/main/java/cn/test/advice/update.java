ed251407621ed4afd02d132429ddd4d3b3873d52
MybeautifulSunShine <m18234818170@163.com>
2018-02-06 15:30:47 +0800

    github修改测试

---
+++ src/main/java/cn/test/advice/update.java
@@ -0,0 +1,10 @@
+package cn.test.advice;
+
+/**
+ * Created by SJ217110601 on 2018/2/6.
+ */
+public class update {
+    public static void main(String[] args) {
+        System.out.println("修改测试");
+    }
+}
